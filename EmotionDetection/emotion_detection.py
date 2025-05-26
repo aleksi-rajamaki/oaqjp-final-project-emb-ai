0373f1b84861673f0346d70ce8180e9508e18f3c
aleksi-rajamaki <aleksi.rajamaki@gmail.com>
2025-05-26 05:46:31 -0400

    Final commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,28 +5,38 @@ def emotion_detector(text_to_analyse):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = input_json, headers = header)
+    response = requests.post(url, json = input_json, headers = header)   
     formatted_response = response.json()
 
-    print(formatted_response)
+    if response.status_code == 400: 
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+            }
+        return emotions
+    
+    else:
+        anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+        disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+        fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+        joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+        saddness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
 
-    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
-    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
-    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
-    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
-    saddness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+        emotions = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+            'fear': fear_score,
+        'joy': joy_score,
+        'sadness': saddness_score
+        }
 
-    emotions = {
-    'anger': anger_score,
-    'disgust': disgust_score,
-    'fear': fear_score,
-    'joy': joy_score,
-    'sadness': saddness_score
-    }
+        emotions["dominant_emotion"] = max(emotions, key=emotions.get)
 
-    emotions["dominant_emotion"] = max(emotions, key=emotions.get)
-
-    return emotions
+        return emotions
 
 
 
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+""" Initiates the Flask application of "NLP - Emotion Detection" on localhost:5000
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,9 +8,14 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analyser():
+    """ Analyses given text. Returns scores for each emotion and dominant emotion. """
+
     text_to_analyse = request.args.get("textToAnalyze")
 
     response = emotion_detector(text_to_analyse)
+
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
 
     return (f"For the given statement, the system response is"
         f" 'anger:' {response['anger']}"
@@ -19,9 +27,9 @@ def emotion_analyser():
 
 @app.route("/")
 def render_index_page():
+    """ Renders the index page for "NLP - Emotion Detection" application. """   
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
+    
